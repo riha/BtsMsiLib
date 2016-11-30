e77bcdc381e64a67a0a15cce9e03097478f4c234
Richard Hallgren <richard.hallgren@gmail.com>
2016-11-30 13:43:30 +0100

    Initial checkin

---
+++ Src/BtsMsiLib/Model/MsiVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtsMsiLib.Model
+{
+    public enum MsiVersion
+    {
+        BT2013,BT2013R2,BT2016
+    }
+}
--- Src/BtsMsiLib/Model/Resource.cs
+++ Src/BtsMsiLib/Model/Resource.cs
@@ -19,9 +19,16 @@ namespace BtsMsiLib.Model
         {
             FilePath = filePath;
             Type = type;
-
-            var assembly = Assembly.LoadFrom(filePath);
-            FullName = assembly.GetName().FullName;
+            if (Type == ResourceType.File)
+            {
+                FullName = System.IO.Path.GetFileName(filePath);
+            }
+            else
+            {
+                var assembly = Assembly.LoadFrom(filePath);
+                FullName = assembly.GetName().FullName;
+            }
+            
         }
 
         internal static string GetLuidHash(string luid)
@@ -47,9 +54,11 @@ namespace BtsMsiLib.Model
                     return "System.BizTalk:BizTalkAssembly";
                 case ResourceType.Resource:
                     return "System.BizTalk:Assembly";
+                case ResourceType.File:
+                    return "System.BizTalk:File";
             }
 
-            throw new NotImplementedException("Unsupported resource type. Currently only BizTalkAssemblies and Assemblies are supported.");
+            throw new NotImplementedException("Unsupported resource type. Currently only File, BizTalkAssemblies and Assemblies are supported.");
         }
 
         internal string GetResourceFolder(string applicationFolder)
@@ -58,7 +67,16 @@ namespace BtsMsiLib.Model
 
             string resourceType = name.Substring(name.IndexOf(':') + 1);
             string resourceTypeFolder = GetResourceTypeFolder(applicationFolder, resourceType);
-            string filename = GetLuidHash(FullName);
+            string filename = "";
+            if (resourceType == "BizTalkAssembly" || resourceType == "Assembly")
+            {
+                filename = GetLuidHash(FullName);    
+            }
+            else
+            {
+                filename = FullName;
+            }
+            
             string luidFilename = FileHelper.GetLuidFilename(filename);
 
             return Path.Combine(resourceTypeFolder, luidFilename);
